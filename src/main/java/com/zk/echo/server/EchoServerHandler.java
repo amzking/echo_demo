ac590c21200e6a32ff863c0679ba5678665bef12
amzking <amzking@foxmail.com>
2018-08-19 10:19:04 +0800

    Initial commit

---
+++ src/main/java/com/zk/echo/server/EchoServerHandler.java
@@ -0,0 +1,11 @@
+package com.zk.echo.server;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * 用于响应入站时间的方法
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    
+}
