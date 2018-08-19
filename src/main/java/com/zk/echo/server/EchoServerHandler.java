ac0d7bb2f5fbae50d1e509051b64c67ec5dbdf74
amzking <amzking@foxmail.com>
2018-08-19 10:40:08 +0800

    update

--- src/main/java/com/zk/echo/server/EchoServerHandler.java
+++ src/main/java/com/zk/echo/server/EchoServerHandler.java
@@ -1,11 +1,18 @@
 package com.zk.echo.server;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 
 /**
  * 用于响应入站时间的方法
  */
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
-    
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf)msg;
+        System.out.println("Server received: " + in.toString(CharsetUtil.UTF_8));
+    }
 }
