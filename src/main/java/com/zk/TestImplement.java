822ca9b606688d9b6c07a170b4d317477c42c545
amzking <amzking@foxmail.com>
2018-09-24 11:17:41 +0800

    update

--- src/main/java/com/zk/TestImplement.java
+++ src/main/java/com/zk/TestImplement.java
@@ -2,5 +2,20 @@ package com.zk;
 
 public class TestImplement implements TestInterface {
 
+    public static void main(String[] args) {
 
+        String str = "a.b.c.d..k,l,";
+        String[] strArray = str.split("\\.|,");
+        System.out.println(strArray.length);
+        for (String s : strArray) {
+            System.out.println(s);
+        }
+
+        String[] strArray1 = str.split("\\.|," , -1);
+        System.out.println(strArray1.length);
+        for (String s : strArray1) {
+            System.out.println(s);
+        }
+
+    }
 }
